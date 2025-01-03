2f64414e3322b0aebb035b75ac8e892177d6c14e
unknown <you@example.com>
2025-01-03 10:19:22 +0430

    Fix execute permissions for app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,6 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 // routes
 app.use('/api/chatbot', require('./routes/chatbot'));
 
-
 // other routes by serving the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
@@ -19,4 +18,7 @@ app.get('*', (req, res) => {
 
 // server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log("now app is listning");
+})
+// app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
